0ca531b876f34bf79d7f243f9805c231c7f0cc39
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-18 11:54:27 +0100

    organize folders and files

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 // React
 import React, { useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// Components
+import Footer from './Footer';
+// Styles
 import '../stylesheets/App.scss';
+import '../stylesheets/Reset.scss';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <p> probando</p>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
