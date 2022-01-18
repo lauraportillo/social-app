551586af2e0d2d0b847a239c39ad68ff344dfb0b
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-18 12:32:54 +0100

    add structure to home component

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@
 import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 // Components
-import Footer from './Footer';
+import Home from './Home';
 // Styles
 import '../stylesheets/App.scss';
 import '../stylesheets/Reset.scss';
 
 function App() {
   return (
-    <div>
-      <p> probando</p>
-      <Footer />
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+// Components
+import Footer from './Footer';
+// Styles
+import '../stylesheets/Home.scss';
+
+const Home = (props) => {
+  return (
+    <div className="containerHome">
+      <header className="nav">login</header>
+
+      <main className="home">
+        <h1>home</h1>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
