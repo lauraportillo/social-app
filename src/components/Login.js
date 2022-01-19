bc728ac881e6e2b013f8596abd65883147c6d01c
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-19 12:35:51 +0100

    add state to login compo

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,13 @@
 // React
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 // Styles
 import '../stylesheets/Login.scss';
 
-const Login = (props) => {
+const Login = () => {
+  //state
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <>
       <div>LOGIN</div>
