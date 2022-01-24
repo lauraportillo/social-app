ca13c193230d17389ad04d294018685c7c1f91c0
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-24 16:03:27 +0100

    render article

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,9 +5,11 @@ import '../stylesheets/Post.scss';
 
 const Post = (props) => {
   return (
-    <>
-      <div>hola</div>
-    </>
+    <article>
+      <img src={props.post.post.photo} alt="post" />
+      <h2>{props.post.name}</h2>
+      <p>{props.post.post.description}</p>
+    </article>
   );
 };
 
