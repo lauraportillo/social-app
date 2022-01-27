d14662cb91bf7ba2e38f34ce7f86baee47bd4340
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-27 13:42:15 +0100

    create new compo UserNotFound

---
+++ src/components/UserNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/UserNotFound.scss';
+
+const UserNotFound = (props) => {
+  return (
+    <>
+      <div>NOT FOUND</div>
+    </>
+  );
+};
+
+export default UserNotFound;
