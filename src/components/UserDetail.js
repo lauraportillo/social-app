869c506366aeb762b4063039ea5bdf3e55f8c6fa
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-28 12:11:22 +0100

    resize profile photos

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -8,11 +8,17 @@ import '../stylesheets/UserDetail.scss';
 
 const UserDetail = (props) => {
   return (
-    <article className="containerUser">
-      <img className="containerUser__img" src={props.post.image} alt="user-pic" />
-      <h2 className="containerUser__title">@{props.post.name}</h2>
-      <p className="containerUser__text">{props.post.description}</p>
-    </article>
+    <div className="containerUser">
+      <HeaderPost />
+      <main>
+        <article className="containerUserArt">
+          <img className="containerUserArt__img" src={props.post.image} alt="user-pic" />
+          <h2 className="containerUserArt__title">@{props.post.name}</h2>
+          <p className="containerUserArt__text">{props.post.description}</p>
+        </article>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
