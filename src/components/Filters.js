64a1eb19da361929daddeb681fcde8533e773080
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-31 21:32:56 +0100

    create filters component to divide postlist

---
+++ src/components/Filters.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/Filters.scss';
+
+const Filters = (props) => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Filters;
