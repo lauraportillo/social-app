ab5617ef2b8753a4e927c2d1312e9b6a0da1c6e1
Laura Portillo <lauraportillo.ark@gmail.com>
2022-01-31 22:46:14 +0100

    add conditional user not found

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -2,6 +2,7 @@
 import Header from './Header';
 import Post from './Post';
 import FilterByName from './FilterByName';
+import UserNotFound from './UserNotFound';
 import Footer from './Footer';
 // Styles
 import '../stylesheets/PostList.scss';
@@ -17,6 +18,10 @@ const PostList = (props) => {
       </li>
     );
   });
+
+  if (renderPost.length === 0) {
+    return <UserNotFound />;
+  }
 
   return (
     <div className="containerPostList">
