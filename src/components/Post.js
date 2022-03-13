398700d6b378a0fea569f9958a6f10885466a337
Laura Portillo <lauraportillo.ark@gmail.com>
2022-03-13 18:03:22 +0100

    add btn heart

--- src/components/Post.js
+++ src/components/Post.js
@@ -7,10 +7,12 @@ const Post = (props) => {
   return (
     <article className="containerPost">
       <img className="containerPost__img" src={props.post.post.photo} alt="post" />
-
-      <Link to={`./post/${props.post.id}`}>
-        <h2 className="containerPost__title">@{props.post.name}</h2>
-      </Link>
+      <div className="containerPost__section1">
+        <Link to={`./post/${props.post.id}`}>
+          <h2 className="containerPost__section1--title">@{props.post.name}</h2>
+        </Link>
+        <button className="containerPost__section1--btn">❤</button>
+      </div>
 
       <p className="containerPost__text">{props.post.post.description}</p>
     </article>
